1db7145f8785e3766ee596f66726446e2c69a25a
machalan <84385784+machalan@users.noreply.github.com>
2021-05-18 18:19:09 +0200

    Update recup_info.py

--- recup_info.py
+++ recup_info.py
@@ -5,29 +5,43 @@ c = wmi.WMI()
 my_system = c.Win32_ComputerSystem()[0]
 info_bios = c.Win32_BIOS()[0]
 carte_mere = c.Win32_BaseBoard ()[0]
+
+# Qui a fabriqué le poste
  
 print(f"Manufacturer: {my_system.Manufacturer}")
 print(f"Marque: {info_bios.Version}")
 print(f"Modele: {my_system. Model}")
 
+# Qui je suis ? (Le poste)
 
 print(f"Hostname: {my_system.Name}")
 print(f"Numéro de série: {info_bios.SerialNumber}")
 
+# Quelle est mon OS ?
 
 print(f"SystemFamily: {my_system.SystemFamily}")
 print(f"SystemType: {my_system.SystemType}")
 
+# Quelles sont mes processeurs ?
 
 print(f"NumberOfProcessors: {my_system.NumberOfProcessors}")
 print(f"Nbr processeur logique: {my_system.NumberOfLogicalProcessors}")
+
+# Quelles est la taille de ma RAM ?
+
 print(f"Taille total de la RAM: {my_system.TotalPhysicalMemory}")
 
 
+# Quelles est mon nom domaine
+
 print(f"Nom de domaine: {my_system.Domain}")
+
+# Qui je sui ? (User)
+
 print(f"Nom de l'utilisateur actuel: {my_system.UserName}")
 
 
+# Quelle est ma carte mere ?
 
 print(f"Type de carte Mere: {carte_mere.Caption}")
 print(f"Fabricant de la carte mere: {carte_mere.Manufacturer}")
@@ -74,7 +88,7 @@ print("")
 #  print (s)
 
 c = wmi.WMI ()
-for s in c.Win32_BaseBoard ():
+for s in c.Win32_Processor ():
    print (s)
 
 
