8db8f02f53d52c3f6c8e8369f6499d6a82f3afc9
machalan <84385784+machalan@users.noreply.github.com>
2021-05-24 23:16:25 +0200

    Update recup_info.py

--- recup_info.py
+++ recup_info.py
@@ -18,24 +18,20 @@ def mes_infos():
 	uname = platform.uname()   
 	my_system = c.Win32_ComputerSystem()[0]
 	info_bios = c.Win32_BIOS()[0]
-	carte_mere = c.Win32_BaseBoard ()[0]
+	carte_mere = c.Win32_BaseBoard()[0]
 	processeur = c.Win32_Processor()[0]
-	memoire_vive_1 = c.Win32_PhysicalMemory ()[0]
-	memoire_vive_2 = c.Win32_PhysicalMemory ()[1]
-	#memoire_vive_3 = c.Win32_PhysicalMemory ()[2]
-	#memoire_vive_4 = c.Win32_PhysicalMemory ()[3]
-	disques_1 = c.Win32_DiskDrive ()[0]
-	disques_2 = c.Win32_DiskDrive ()[1]
-	adaptateur_resaux_1 = c.Win32_NetworkAdapter ()[0]
-	adaptateur_resaux_2 = c.Win32_NetworkAdapter ()[1]
-	adaptateur_resaux_3 = c.Win32_NetworkAdapter ()[2]
-	adaptateur_resaux_4 = c.Win32_NetworkAdapter ()[3]
+	memoire_vive = c.Win32_PhysicalMemory()
+	mes_disques = c.Win32_DiskDrive()
+	mes_cartes_reseaux = c.Win32_NetworkAdapter()
 	ip_local = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	ip_local.connect(("8.8.8.8", 80))
 	mon_ip_local = ip_local.getsockname()[0]
 	ip_local.close()
 	ip2 = requests.get('https://checkip.amazonaws.com').text.strip()
 	info_du_poste = {}
+	RAM = {}
+	disque = {}
+	cartes_reseaux = {}
 	info_du_poste['Mon IP local'] = mon_ip_local
 	info_du_poste["Mon IP public"] = ip2
 	info_du_poste["Fabricant:"] = my_system.Manufacturer
@@ -54,267 +50,61 @@ def mes_infos():
 	info_du_poste["NumberOfProcessors:"] = my_system.NumberOfProcessors
 	info_du_poste["Nbr processeur logique:"] = processeur.NumberOfLogicalProcessors
 	info_du_poste["Taille total de la RAM:"] = my_system.TotalPhysicalMemory
-	memoire_vive_1_Go = (int(memoire_vive_1.Capacity)/1073741824)
-	info_du_poste["RAM-1 Taille:"] = (round(memoire_vive_1_Go))
-	info_du_poste["RAM-1 Vitesse:"] = memoire_vive_1.ConfiguredClockSpeed
-	info_du_poste["RAM-1 Fabricant ou code Fabricant:"] = memoire_vive_1.Manufacturer
-	info_du_poste["RAM-1 Numero de serie :"] = memoire_vive_1.SerialNumber
-	info_du_poste["RAM-1 Tag :"] = memoire_vive_1.Tag
-	memoire_vive_2_Go = (int(memoire_vive_2.Capacity)/1073741824)
-	info_du_poste["RAM-2 Taille:"] = (round(memoire_vive_2_Go))
-	info_du_poste["RAM-2 Vitesse:"] = memoire_vive_2.ConfiguredClockSpeed
-	info_du_poste["RAM-2 Fabricant ou code Fabricant:"] = memoire_vive_2.Manufacturer
-	info_du_poste["RAM-2 Numero de serie :"] = memoire_vive_2.SerialNumber
-	info_du_poste["RAM-2 Tag :"] = memoire_vive_2.Tag
-	info_du_poste["disk 1 - Model du disque :"] = disques_1.Model
-	info_du_poste["disk 1 - Type interface :"] = disques_1.InterfaceType
-	info_du_poste["disk 1 - Numero de serie :"] = disques_1.SerialNumber
-	disques_1_taille_Go = (int(disques_1.Size)/1073741824)
-	info_du_poste["disk 1 - Taille du disque :"] = round(disques_1_taille_Go)
-	info_du_poste["disk 1 - OK ou defaillant :"] = disques_1.Status
-	info_du_poste["disk 2 - Model du disque :"] = disques_2.Model
-	info_du_poste["disk 2 - Type interface :"] = disques_2.InterfaceType
-	info_du_poste["disk 2 - Numero de serie :"] = disques_2.SerialNumber
-	disques_2_taille_Go = (int(disques_2.Size)/1073741824)
-	info_du_poste["disk 2 - Taille du disque :"] = round(disques_2_taille_Go)
-	info_du_poste["disk 2 - OK ou defaillant :"] = disques_2.Status
 	info_du_poste["Nom de domaine:"] = my_system.Domain
 	info_du_poste["Nom de l'utilisateur actuel:"] = my_system.UserName
 	info_du_poste["Type de carte Mere:"] = carte_mere.Caption
 	info_du_poste["Fabricant de la carte mere:"] = carte_mere.Manufacturer
 	info_du_poste["Carte mere remplacable:"] = carte_mere.Replaceable
 	info_du_poste["Numero de serie de la carte mere:"] = carte_mere.SerialNumber
-	info_du_poste["Carte reseaux-1 :"] = adaptateur_resaux_1.AdapterType
-	info_du_poste["Carte reseaux-1 - Description :"] = adaptateur_resaux_1.Description
-	info_du_poste["Carte reseaux-1 - MACAddress :"] = adaptateur_resaux_1.MACAddress
-	info_du_poste["Carte reseaux-2 :"] = adaptateur_resaux_2.AdapterType
-	info_du_poste["Carte reseaux-2 - Description : "] = adaptateur_resaux_2.Description
-	info_du_poste["Carte reseaux-2 - MACAddress :"] = adaptateur_resaux_2.MACAddress
-	info_du_poste["Carte reseaux-3 :"] = adaptateur_resaux_3.AdapterType
-	info_du_poste["Carte reseaux-3 - Description :"] = adaptateur_resaux_3.Description
-	info_du_poste["Carte reseaux-3 - MACAddress :"] = adaptateur_resaux_3.MACAddress
-	info_du_poste["Carte reseaux-4 :"] = adaptateur_resaux_4.AdapterType
-	info_du_poste["Carte reseaux-4 - Description :"] = adaptateur_resaux_4.Description
-	info_du_poste["Carte reseaux-4 - MACAddress :"] = adaptateur_resaux_4.MACAddress
 	with open('C:\sources\data.json', 'w') as fp:
 		  fp.write(json.dumps(info_du_poste, indent=4))
+	
+	for memoire_vive in memoire_vive:
+		print(memoire_vive.Capacity)
+		memoire_vive_1_Go = (int(memoire_vive.Capacity)/1073741824)
+		RAM["RAM Taille:"] = (round(memoire_vive_1_Go))
+		RAM["RAM Vitesse:"] = memoire_vive.ConfiguredClockSpeed
+		RAM["RAM Fabricant ou code Fabricant:"] = memoire_vive.Manufacturer
+		RAM["RAM Numero de serie :"] = memoire_vive.SerialNumber
+		RAM["RAM Tag :"] = memoire_vive.Tag
+		with open('C:\sources\data.json', 'a+') as fp:
+			fp.write(json.dumps(RAM, indent=4))
 
-def afficher_mes_infos():  # N'est utile que pour debuguer
-	c = wmi.WMI()
-	uname = platform.uname()   
-	my_system = c.Win32_ComputerSystem()[0]
-	info_bios = c.Win32_BIOS()[0]
-	carte_mere = c.Win32_BaseBoard ()[0]
-	processeur = c.Win32_Processor()[0]
-	memoire_vive_1 = c.Win32_PhysicalMemory ()[0]
-	memoire_vive_2 = c.Win32_PhysicalMemory ()[1]
-	#memoire_vive_3 = c.Win32_PhysicalMemory ()[2]
-	#memoire_vive_4 = c.Win32_PhysicalMemory ()[3]
-	disques_1 = c.Win32_DiskDrive ()[0]
-	disques_2 = c.Win32_DiskDrive ()[1]
-	adaptateur_resaux_1 = c.Win32_NetworkAdapter ()[0]
-	adaptateur_resaux_2 = c.Win32_NetworkAdapter ()[1]
-	adaptateur_resaux_3 = c.Win32_NetworkAdapter ()[2]
-	adaptateur_resaux_4 = c.Win32_NetworkAdapter ()[3]
-	ip_local = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	ip_local.connect(("8.8.8.8", 80))
-	mon_ip_local = ip_local.getsockname()[0]
-	ip_local.close()
-	ip2 = requests.get('https://checkip.amazonaws.com').text.strip() ##
-	print("")
-	print("="*20, "Information sur la machine", "="*20)
-	print("")
+	for mes_disques in mes_disques:
+		print(mes_disques.Model)
+		disques_1_taille_Go = (int(mes_disques.Size)/1073741824)
+		disque["Disque - Taille du disque :"] = round(disques_1_taille_Go)
+		disque["Disque - OK ou defaillant :"] = mes_disques.Status
+		disque["Disque - Model du disque :"] = mes_disques.Model
+		disque["Disque - Type interface :"] = mes_disques.InterfaceType
+		disque["Disque - Numero de serie :"] = mes_disques.SerialNumber
+		with open('C:\sources\data.json', 'a+') as fp:
+			fp.write(json.dumps(disque, indent=4))
 
-	# Qui a fabriqué le poste
-
-	print("")
-	print("="*20, "Qui a fabriqué le poste ?", "="*20)
-	print("")
-	 
-	print(f"Manufacturer: {my_system.Manufacturer}")
-	print(f"Marque: {info_bios.Version}")
-	print(f"Modele: {my_system.Model}")
-	print("")
-
-
-	# Qui je suis ? (Le poste)
-
-	print("")
-	print("="*20, "Qui je suis ? (Le poste)", "="*20)
-	print("")
-
-	print(f"Hostname: {my_system.Name}")
-	print(f"Numéro de série: {info_bios.SerialNumber}")
-	print("")
-
-
-	# Quelle est mon OS ?
-
-	print("")
-	print("="*20, "Quelle est mon OS ?", "="*20)
-	print("")
-
-	print(f"System: {uname.system}")           #  Returns the system/OS name
-	print(f"Windows: {uname.release}")          # Returns the system’s release
-	print(f"Version de Windows: {uname.version}")        #  Returns the system’s release version, e.
-	print(f"Architecture Machine: {uname.machine}")               #  Returns the machine type, e.g. 'i386'.
-	print(f"SystemFamily: {my_system.SystemFamily}")
-	print(f"SystemType: {my_system.SystemType}")
-	print ("Pocessor: " +platform.processor())
-	print("")
-
-	# Quelles sont mes processeurs ?
-
-	print("")
-	print("="*20, "Quelles sont mes processeurs ?", "="*20)
-	print("")
-
-	print(f"Nom du processeur: {processeur.Name}")
-	print(f"NumberOfProcessors: {my_system.NumberOfProcessors}")
-	print(f"Nbr processeur logique: {processeur.NumberOfLogicalProcessors}")
-	print("")
-
-	# Quelles est la taille de ma RAM ?
-
-	print("")
-	print("="*20, "Quelles est la taille de ma RAM ?", "="*20)
-	print("")
-
-	print(f"Taille total de la RAM: {my_system.TotalPhysicalMemory}")
-	print("")
-	print(f"RAM-1 Taille: {memoire_vive_1.Capacity}")
-	memoire_vive_1_Go = (int(memoire_vive_1.Capacity)/1073741824)
-	print(f"RAM-1 - Taille de la en Go: {round(memoire_vive_1_Go)}")
-	print(f"RAM-1 Vitesse: {memoire_vive_1.ConfiguredClockSpeed}")
-	print(f"RAM-1 Fabricant ou code Fabricant: {memoire_vive_1.Manufacturer}")
-	print(f"RAM-1 Numéro de serie : {memoire_vive_1.SerialNumber}")
-	print(f"RAM-1 Tag : {memoire_vive_1.Tag}")
-	print("")
-	print(f"RAM-2 Taille: {memoire_vive_2.Capacity}")
-	memoire_vive_2_Go = (int(memoire_vive_2.Capacity)/1073741824)
-	print(f"RAM-2  - Taille en Go: {round(memoire_vive_2_Go)}")
-	print(f"RAM-2 Vitesse: {memoire_vive_2.ConfiguredClockSpeed}")
-	print(f"RAM-2 Fabricant ou code Fabricant: {memoire_vive_2.Manufacturer}")
-	print(f"RAM-2 Numéro de serie : {memoire_vive_2.SerialNumber}")
-	print(f"RAM-2 Tag : {memoire_vive_2.Tag}")
-	print("")
-
-
-
-	# Quelles est la taille de mon ou mes disques ?
-
-	print("")
-	print("="*20, "Quelles est la taille de mon ou mes disques ?", "="*20)
-	print("")
-
-	print("")
-	print(f"disk 1 - Model du disque : {disques_1.Model}")
-	print(f"disk 1 - Type interface : {disques_1.InterfaceType}")
-	print(f"disk 1 - Numéro de série : {disques_1.SerialNumber}")
-	print(f"disk 1 - Taille du disque : {disques_1.Size}")
-	disques_1_taille_Go = (int(disques_1.Size)/1073741824)
-	print(f"disk 1 - Taille du disque en Go: {round(disques_1_taille_Go)}")
-	print(f"disk 1 - OK ou défaillant : {disques_1.Status}")
-	print("")
-	print(f"disk 2 - Model du disque : {disques_2.Model}")
-	print(f"disk 2 - Type interface : {disques_2.InterfaceType}")
-	print(f"disk 2 - Numéro de série : {disques_2.SerialNumber}")
-	print(f"disk 2 - Taille du disque : {disques_2.Size}")
-	disques_2_taille_Go = (int(disques_2.Size)/1073741824)
-	print(f"disk 2 - Taille du disque en Go: {round(disques_2_taille_Go)}")
-	print(f"disk 2 - OK ou défaillant : {disques_2.Status}")
-	print("")
+	for mes_cartes_reseaux in mes_cartes_reseaux:
+		print(mes_cartes_reseaux.Description)
+		cartes_reseaux["Carte reseaux :"] = mes_cartes_reseaux.AdapterType
+		cartes_reseaux["Carte reseaux - Description :"] = mes_cartes_reseaux.Description
+		cartes_reseaux["Carte reseaux - MACAddress :"] = mes_cartes_reseaux.MACAddress
+		with open('C:\sources\data.json', 'a+') as fp:
+			fp.write(json.dumps(cartes_reseaux, indent=4))
 
 
 
 
-	# Quelles est mon nom domaine
-
-	print("")
-	print("="*20, "Quelle est mon nom domaine ?", "="*20)
-	print("")
-
-	print(f"Nom de domaine: {my_system.Domain}")
-	print("")
-	# Qui je suis ? (User)
-
-	print("")
-	print("="*20, "Qui je suis ?", "="*20)
-	print("")
-
-	print(f"Nom de l'utilisateur actuel: {my_system.UserName}")
-	print("")
-
-	# Quelle est ma carte mere ?
-
-	print("")
-	print("="*20, "Quelle est ma carte mere ?", "="*20)
-	print("")
-
-	print(f"Type de carte Mere: {carte_mere.Caption}")
-	print(f"Fabricant de la carte mere: {carte_mere.Manufacturer}")
-	print(f"Carte mere remplacable: {carte_mere.Replaceable}")
-	print(f"Numéro de serie de la carte mere: {carte_mere.SerialNumber}")
-	print("")
-
-	# Mes cartes réseaux ?
-
-	print("")
-	print("="*20, "Quelle sont mes cartes réseaux ?", "="*20)
-	print("")
-
-	print(f"Carte réseaux-1 : {adaptateur_resaux_1.AdapterType}")
-	print(f"Carte réseaux-1 - Description : {adaptateur_resaux_1.Description}")
-	print(f"Carte réseaux-1 - MACAddress : {adaptateur_resaux_1.MACAddress}")
-	print("")
-	print(f"Carte réseaux-2 : {adaptateur_resaux_2.AdapterType}")
-	print(f"Carte réseaux-2 - Description : {adaptateur_resaux_2.Description}")
-	print(f"Carte réseaux-2 - MACAddress : {adaptateur_resaux_2.MACAddress}")
-	print("")
-	print(f"Carte réseaux-3 : {adaptateur_resaux_3.AdapterType}")
-	print(f"Carte réseaux-3 - Description : {adaptateur_resaux_3.Description}")
-	print(f"Carte réseaux-3 - MACAddress : {adaptateur_resaux_3.MACAddress}")
-	print("")
-	print(f"Carte réseaux-4 : {adaptateur_resaux_4.AdapterType}")
-	print(f"Carte réseaux-4 - Description : {adaptateur_resaux_4.Description}")
-	print(f"Carte réseaux-4 - MACAddress : {adaptateur_resaux_4.MACAddress}")
-	print("")
 
 
-	# Mon IP local avec laquelle je vais sur internet
 
-	print("")
-	print("="*20, "Quelle est mon ip local ?", "="*20)
-	print("")
-
-
-	ip_local = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	ip_local.connect(("8.8.8.8", 80))
-	mon_ip_local = ip_local.getsockname()[0]
-	ip_local.close()
-
-	print("Mon IP Local est : " +mon_ip_local)
-	print("")
-
-	# Mon IP public
-
-	print("")
-	print("="*20, "Quelle est mon ip Public ?", "="*20)
-	print("")
-
-	ip2 = requests.get('https://checkip.amazonaws.com').text.strip()
-	print("Mon IP Public est : " +ip2)
-	print("")
 
 
 # Executer mon script
 
 # Lancer la collecte, afficher la collect, attendre 45s et recommencer
 i = 1
-while i <= 5:
-	mes_infos()
-	afficher_mes_infos()
-	print ("voila!")   
+#while i <= 5:
+mes_infos()
+
+print ("voila!")   
 
 
 
